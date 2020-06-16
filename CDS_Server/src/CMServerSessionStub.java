0b5e3b33feca483c3343d9119d4f92f1bade419a
hsoh0306 <43984708+hsoh0306@users.noreply.github.com>
2020-06-16 13:52:48 +0900

    merge

---
+++ CDS_Server/src/CMServerSessionStub.java
@@ -0,0 +1,48 @@
+
+public class CMServerSessionStub {
+	private String[] streamers;
+	final int MAXSTREAMER = 5;
+	final String initStr = ".";
+	
+	public CMServerSessionStub() {
+		streamers = new String[MAXSTREAMER];
+		for(int i = 0; i < MAXSTREAMER; ++i) {
+			streamers[i] = initStr;
+		}
+	}
+	
+	public String getStreamerID() {
+		String ret = "";
+		for(int i = 0; i < MAXSTREAMER; ++i) {
+			ret += streamers[i]+"@@";
+		}
+		System.out.println("getStreamerID: "+ret);
+		return ret;
+	}
+	private void setStreamerID(int num, String streamerID) {
+		this.streamers[num] = streamerID;
+	}
+	public String leaveSession(String streamerID) {
+		for(int i = 0; i < MAXSTREAMER; ++i) {
+			if(streamers[i].equals(streamerID)) {
+				streamers[i] = initStr;
+				i++;
+				return "session"+i;
+			}
+		}
+		return "";
+	}
+	public String getPossibleSession(String senderID) {
+		String ret = initStr;
+		for(int i = 0; i < MAXSTREAMER; ++i) {
+			if(streamers[i].equals(initStr)) {
+				System.out.println("streamers["+i+"] == .");
+				setStreamerID(i, senderID);
+				i++;
+				ret = "session"+i;
+				break;
+			}
+		}
+		return ret;
+	}
+}
